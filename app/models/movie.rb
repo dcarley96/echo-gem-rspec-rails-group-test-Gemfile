48c0dcb70faad5ed5c2148cab53ea0168e922053
dimitri carley <dcarley@gatling.io>
2022-01-06 18:58:28 +0100

    push first action of exo 2

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,6 +1,5 @@
 class Movie < ApplicationRecord
-  belongs_to :movie
-  belongs_to :list
   has_many :bookmarks
   validates :title, uniqueness: true, presence: true
+  validates :overview, presence: true
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,27 +10,36 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_12_18_114008) do
+ActiveRecord::Schema.define(version: 2021_12_18_140435) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "bookmarks", force: :cascade do |t|
+    t.text "comment"
+    t.bigint "movie_id", null: false
+    t.bigint "list_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.index ["list_id"], name: "index_bookmarks_on_list_id"
+    t.index ["movie_id"], name: "index_bookmarks_on_movie_id"
   end
 
   create_table "lists", force: :cascade do |t|
+    t.string "name"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
 
-  create_table "titles", force: :cascade do |t|
-    t.string "overview"
+  create_table "movies", force: :cascade do |t|
+    t.string "title"
+    t.text "overview"
     t.string "poster_url"
-    t.string "rating"
+    t.integer "rating"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
 
+  add_foreign_key "bookmarks", "lists"
+  add_foreign_key "bookmarks", "movies"
 end
